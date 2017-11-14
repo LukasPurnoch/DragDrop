d4e09419556c910eb35ab5d89bb5dd1196180b8d
purnolu15 <purnolu15@DST20712>
2017-11-14 10:45:44 +0100

    init

---
+++ DAD/DAD/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FileHelpers;
+
+namespace DAD
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Move> pos = new List<Move>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CSVWrite()
+        {
+            var engine = new FileHelperEngine<Move>();
+            engine.WriteFile("save_pos.csv", pos);
+        }
+
+        //((Rectangle)sender).CaptureMouse();
+
+        private bool _isRectDragInProg;
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = true;
+            ((Rectangle)sender).CaptureMouse();
+        }
+        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = false;
+            ((Rectangle)sender).CaptureMouse();
+        }
+        private void rect_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg) return;
+
+            var mousePos = e.GetPosition(canvas);
+
+            double left = mousePos.X - (rect.ActualWidth / 2);
+            double top = mousePos.Y - (rect.ActualHeight / 2);
+            Canvas.SetLeft(rect, left);
+            Canvas.SetTop(rect, top);
+        }
+
+        /*private bool _isRectDragInProg1;
+        private void rect_MouseLeftButtonDown1(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg1 = true;
+            rect1.CaptureMouse();
+        }
+        private void rect_MouseLeftButtonUp1(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg1 = false;
+            rect1.ReleaseMouseCapture();
+        }
+        private void rect_MouseMove1(object sender, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg1) return;
+
+            var mousePos = e.GetPosition(canvas);
+
+            double left = mousePos.X - (rect1.ActualWidth / 2);
+            double top = mousePos.Y - (rect1.ActualHeight / 2);
+            Canvas.SetLeft(rect1, left);
+            Canvas.SetTop(rect1, top);
+
+            Move set = new Move();
+            set.LEFT = left;
+            set.TOP = top;
+            
+            pos.Add(set);
+
+            CSVWrite();
+        }*/
+    }
+}
---
+++ DAD/DAD/Move.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FileHelpers;
+
+namespace DAD
+{
+    [DelimitedRecord(",")]
+    class Move
+    {
+
+        public string Name;
+        public double LEFT;
+        public double TOP;
+
+        private bool _isRectDragInProg;
+
+        public void MBDown(Rectangle rect)
+        {
+            _isRectDragInProg = true;
+            rect.CaptureMouse();
+        }
+        public void MBUp(Rectangle rect)
+        {
+            _isRectDragInProg = false;
+            rect.ReleaseMouseCapture();
+        }
+        public void MMove(Rectangle rect, Canvas canvas, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg) return;
+
+            var mousePos = e.GetPosition(canvas);
+
+            LEFT = mousePos.X - (rect.ActualWidth / 2);
+            TOP = mousePos.Y - (rect.ActualHeight / 2);
+            Canvas.SetLeft(rect, LEFT);
+            Canvas.SetTop(rect, TOP);
+        }
+    }
+}
