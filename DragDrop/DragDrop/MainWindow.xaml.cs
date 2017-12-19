1ff2b8ec7aadd2f71751505d34376555d0694f2c
purnolu15 <purnolu15@DST20712>
2017-12-19 08:30:26 +0100

    drag drop

--- DragDrop/DragDrop/MainWindow.xaml.cs
+++ DragDrop/DragDrop/MainWindow.xaml.cs
@@ -21,23 +21,64 @@ namespace DragDrop
     public partial class MainWindow : Window
     {
 
+        bool drag = false;
+        Point startPoint;
 
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        // this creates and adds rectangles dynamically
+        private void addRectangleButton_Click(object sender, RoutedEventArgs e)
+        {
+            // create new Rectangle
+            Rectangle rect = new Rectangle();
+            // assign properties
+            rect.Width = 100;
+            rect.Height = 50;
+            rect.Fill = new SolidColorBrush(Colors.RoyalBlue);
+            // assign handlers
+            rect.MouseDown += rect_MouseDown;
+            rect.MouseMove += rect_MouseMove;
+            rect.MouseUp += rect_MouseUp;
+            // set default position
+            Canvas.SetLeft(rect, 0);
+            Canvas.SetTop(rect, 0);
+            // add it to canvas
+            canvas.Children.Add(rect);
+        }
+
+        private void rect_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // start dragging
+            drag = true;
+            // save start point of dragging
+            startPoint = Mouse.GetPosition(canvas);
         }
 
         private void rect_MouseMove(object sender, MouseEventArgs e)
         {
-            Rectangle rectangle = sender as Rectangle;
-            if (rectangle != null && e.LeftButton == MouseButtonState.Pressed)
+            // if dragging, then adjust rectangle position based on mouse movement
+            if (drag)
             {
-                DragDrop.DoDragDrop(rectangle,
-                                     rectangle.Fill.ToString(),
-                                     DragDropEffects.Copy);
+                Rectangle draggedRectangle = sender as Rectangle;
+                Point newPoint = Mouse.GetPosition(canvas);
+                double left = Canvas.GetLeft(draggedRectangle);
+                double top = Canvas.GetTop(draggedRectangle);
+                Canvas.SetLeft(draggedRectangle, left + (newPoint.X - startPoint.X));
+                Canvas.SetTop(draggedRectangle, top + (newPoint.Y - startPoint.Y));
+
+                startPoint = newPoint;
             }
         }
 
+        private void rect_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // stop dragging
+            drag = false;
+        }
+
     }
+
 }
